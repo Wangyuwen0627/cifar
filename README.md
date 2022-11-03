b3f5df436a88ccf4f98f0e7d1d2c5c333bf68e66
BJTU18301018 <2034161581@qq.com>
2022-11-03 22:45:51 +0800

    modify

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
+# CIFAR10分类
+## 1. 模型结构介绍
+### 1.1 ResNet-18
+![img.png](pics/model_struct/resnet18.png)
+如图所示，ResNet网络是参考了VGG19网络，并且在其基础上进行了修改，主要改进在于通过短路机制加入了残差单元。
+如图中，ResNet相比普通网络每两层间增加了短路机制，虚线就表示feature map数量发生了改变。  
+ResNet直接使用stride=2的卷积做下采样，并且用global average pool层替换了全连接层。
+当feature map大小降低一半时，feature map的数量增加一倍，这保持了网络层的复杂度。  
+ResNet-18的详细结构解析如下：  
+1. 原文中用的图像输入是3*224*224，先通过一个7*7*64的卷积，但是步长设置为2，使得图像的大小缩小了一半；
+2. 在con2_x的刚开始，通过一个最大值池化，步长设置为2，使得图像又缩小了一半；
+3. 然后是con2_x、con3_x、con4_x、con5_x一共8个残差块；
+4. 按照作者说的，在con3_1、con4_1、con5_1都进行了2倍的下采样；
+5. 最后一层先经过一个自适应平均池化层，然后一个全连接层映射到输出；
+### 1.2 DenseNet
+![img.png](pics/model_struct/desnet.png)  
+
+DenseNet不同于ResNet中的残差结构，也不同于GoogLetNet中的Inception网络结构。
+DenseNet通过更密集的特征连接，使模型有了更好的性能，并且相比ResNet进一步减少了参数。  
+DenseNet中有两个重要的结构，分别是Dense Block和Transition Layer
+![img.png](pics/model_struct/denseblock.png)  
+
+上图为DenseNet的Dense Block结构，DenseNet就是由多个这样的Dense Block结构组成，
+它的核心思想就是后面每个层都会接受其前面所有层作为其额外的输入，
+可以看到最后Transition Layer处，所有颜色的线都有，也就是前面所有层都作为输入。  
+Transition layer由BN+Conv(1×1)+2×2 average-pooling组成。
+Transition Layer将不同DenseBlock之间进行连接的模块，主要功能是整合上一个DenseBlock获得的特征，缩小上一个DenseBlock的宽高，达到下采样效果，特征图的宽高减半。
+
+## 2. 模型效果展示
+### 2.1 ResNet-18模型效果展示
+该种方法下，模型测试准确率可以达到：
+### 2.2 DenseNet模型效果展示
+该种方法下，模型测试准确率可以达到：
+## 3. 模型超参数对模型性能的影响
+### 3.1 学习率
+学习率过大导致无法收敛，原因是因为学习率大会导致模型学习数据时间加快，提前到达拟合点，
+但是epoch没结束，继续学习训练数据，容易导致过拟合于原始数据。过小则导致收敛慢。
+### 3.2 batch_size
+模型对batchsize虽然没有学习率那么敏感, 但是进一步提高模型性能上, batchsize会变成一个非常关键的参数。
+大的batchsize减少训练时间, 提高稳定性，但是泛化能力下降，小的batchsize则会使训练时间变长。
+### 3.3 优化器
+选择优化器的问题在于，由于no-free-lunch定理，没有一个单一的优化器可以在所有场景中超越其他的。
+因此没有一劳永逸的解决方案，必须根据手头的特定问题仔细选择优化器。
+根据目前观察目前SOTA所采用的一些优化器，发现广泛使用的是SGD优化器，因此比较了一下使用SGD优化器和Adam、Adagrad几种优化器时模型的性能。
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -13,47 +14,60 @@ from ResNet18.net import ResNet18
 from DenseNet.net import DenseNet
 
 def drawAcc(train_acc_list, test_acc_list):
-    fig, ax = plt.subplots(figsize=(7, 3), dpi=200, facecolor = "#EFE9E6")
+    fig, ax = plt.subplots(figsize=(9, 3), dpi=200, facecolor = "#EFE9E6")
     ax.spines["left"].set_visible(False)
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
     ax.grid(ls="--", lw=0.5, color="#4E616C")
     X = pd.Series(range(len(train_acc_list)))
+    # X = np.linspace(0, len(train_acc_list), 1024)
     print(X)
     print(train_acc_list)
     print(test_acc_list)
-    ax.plot(X, train_acc_list, marker="o", mfc = "white", ms = 5)
-    ax.plot(X, test_acc_list, marker="o", mfc = "white", ms = 5)
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))  # ticker every 2 matchdays
-    xticks_ = ax.xaxis.set_ticklabels([x - 1 for x in range(0, len(X) + 3, 2)])
+    ax.plot(X, train_acc_list, marker="o", mfc = "white", ms = 2)
+    ax.plot(X, test_acc_list, marker="o", mfc = "white", ms = 2)
+    # xmajor = ticker.MultipleLocator(10)  # 主 10 的倍数
+    # xminor = ticker.MultipleLocator(5)  # 次 2的倍数
+    # xticks_ = ax.xaxis.set_ticklabels([x for x in range(0, len(X) + 3, 4)])
     # This last line outputs
     # [-1, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
     # and we mark the tickers every two positions.
-    ax.xaxis.set_tick_params(length=2, color="#4E616C", labelcolor="#4E616C", labelsize=6)
-    ax.yaxis.set_tick_params(length=2, color="#4E616C", labelcolor="#4E616C", labelsize=6)
+    # ax.xaxis.set_tick_params(length=5, color="#4E616C", labelcolor="#4E616C", labelsize=6)
+    # ax.yaxis.set_tick_params(length=5, color="#4E616C", labelcolor="#4E616C", labelsize=6)
+    # ax.xaxis.set_minor_locator(xminor)
+    # ax.xaxis.set_major_locator(xmajor)
+    # ax.xaxis.set_tick_params(color="#4E616C", labelcolor="#4E616C", labelsize=6, which="minor", length=4)
+    # ax.xaxis.set_tick_params(color="#4E616C", labelcolor="#4E616C", labelsize=6, which="major", length=5)
     ax.spines["bottom"].set_edgecolor("#4E616C")
     plt.savefig('/home/wyw/cifar/results/resnet18/resnet18_acc.png')#保存图片
     plt.show()
 
 def drawLoss(train_loss_list):
-    fig, ax = plt.subplots(figsize=(7, 3), dpi=200)
+    fig, ax = plt.subplots(figsize=(9, 3), dpi=200, facecolor = "#EFE9E6")
     ax.spines["left"].set_visible(False)
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
     ax.grid(ls="--", lw=0.5, color="#4E616C")
     X = pd.Series(range(len(train_loss_list)))
-    ax.plot(X, train_loss_list, marker="o", mfc = "white", ms = 5)
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(2))  # ticker every 2 matchdays
-    xticks_ = ax.xaxis.set_ticklabels([x - 1 for x in range(0, len(X) + 3, 2)])
+    ax.plot(X, train_loss_list, marker="o", mfc = "white", ms = 1)
+    xmajor = ticker.MultipleLocator(10)  # 主 10 的倍数
+    xminor = ticker.MultipleLocator(5)  # 次 2的倍数
+    # ax.xaxis.set_major_locator(ticker.MultipleLocator(2))  # ticker every 2 matchdays
+    # xticks_ = ax.xaxis.set_ticklabels([x for x in range(0, len(X) + 3, 4)])
     # This last line outputs
     # [-1, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
     # and we mark the tickers every two positions.
-    ax.xaxis.set_tick_params(length=2, color="#4E616C", labelcolor="#4E616C", labelsize=6)
-    ax.yaxis.set_tick_params(length=2, color="#4E616C", labelcolor="#4E616C", labelsize=6)
+    # ax.xaxis.set_tick_params(length=5, color="#4E616C", labelcolor="#4E616C", labelsize=6)
+    # ax.yaxis.set_tick_params(length=5, color="#4E616C", labelcolor="#4E616C", labelsize=6)
+    ax.xaxis.set_minor_locator(xminor)
+    ax.xaxis.set_major_locator(xmajor)
+    ax.tick_params(axis="x", direction="in", color="#4E616C", labelcolor="#4E616C", labelsize=6, which="minor",
+                   length=4)
+    ax.tick_params(axis="x", direction="out", color="#4E616C", labelcolor="#4E616C", labelsize=6, which="major",
+                   length=5)
     ax.spines["bottom"].set_edgecolor("#4E616C")
     plt.savefig('/home/wyw/cifar/results/resnet18/resnet18_loss.png')  # 保存图片
     plt.show()
-
 
 # 定义是否使用GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -67,7 +81,7 @@ args = parser.parse_args()
 # 超参数设置
 EPOCH = 150  # 遍历数据集次数
 pre_epoch = 0  # 定义已经遍历数据集的次数
-BATCH_SIZE = 64  # 批处理尺寸(batch_size)
+BATCH_SIZE = 128  # 批处理尺寸(batch_size)
 LR = 0.1  # 学习率
 
 # 准备数据集并预处理
@@ -75,12 +89,14 @@ transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
     transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),  # R,G,B每层的归一化用到的均值和方差
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)  # 训练数据集
@@ -93,9 +109,9 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False,
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # 模型定义-ResNet
-# net = ResNet18().to(device)
-net = DenseNet(growthRate=12, depth=100, reduction=0.5,
-               bottleneck=True, nClasses=10).to(device)
+net = ResNet18().to(device)
+# net = DenseNet(growthRate=12, depth=100, reduction=0.5,
+#                bottleneck=True, nClasses=10).to(device)
 
 # 定义损失函数和优化方式
 criterion = nn.CrossEntropyLoss()  # 损失函数为交叉熵，多用于多分类问题
